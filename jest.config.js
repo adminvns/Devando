11de060b57208fdea64a458d6f2239505de07588
adminvns <shubham.editor@gmail.com>
2025-06-14 20:20:20 +0530

    Update Jest and SonarQube configurations to include coverage thresholds and additional exclusions

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,19 @@ module.exports = {
   coverageDirectory: "coverage",
   coverageReporters: ["lcov", "text"],
   testEnvironment: "node",
-   forceExit: true
+  forceExit: true,
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "routes/calculator.js",
+    "server.js"
+  ],
+  // Optional: you can also set coverage thresholds for the remaining files
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
 };
